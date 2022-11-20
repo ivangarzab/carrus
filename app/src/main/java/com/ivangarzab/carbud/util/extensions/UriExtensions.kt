5976be3d7f6562d3e00b27683ae2320687fbc1fc
Ivan Garza <ivangb6@gmail.com>
2022-11-19 18:38:27 -0800

    Some clean up

--- app/src/main/java/com/ivangarzab/carbud/util/extensions/UriExtensions.kt
+++ app/src/main/java/com/ivangarzab/carbud/util/extensions/UriExtensions.kt
@@ -9,34 +9,35 @@ import java.io.*
  * Created by Ivan Garza Bermea.
  */
 
-fun Uri.writeInFile(contentResolver: ContentResolver, content: String) {
-    val outputStream: OutputStream
+fun Uri.writeInFile(contentResolver: ContentResolver, content: String) =
     try {
-        outputStream = contentResolver.openOutputStream(this)!!
-        val bw = BufferedWriter(OutputStreamWriter(outputStream))
-        bw.write(content)
-        bw.flush()
-        bw.close()
-        Timber.v("Successful write to file with uri: $this")
+        contentResolver.openOutputStream(this)!!.let { outputStream ->
+            BufferedWriter(OutputStreamWriter(outputStream)).apply {
+                write(content)
+                flush()
+                close()
+            }
+            Timber.v("Successful write to file with uri: $this")
+        }
     } catch (e: IOException) {
         Timber.w("Unable to write to file with uri: $this", e)
     }
-}
 
 fun Uri.readFromFile(contentResolver: ContentResolver): String? {
     var result = ""
     return try {
-        val inputStream = contentResolver.openInputStream(this)!!
-        val br = BufferedReader(InputStreamReader(inputStream))
-        var strLine: String?
-        while (br.readLine().also {
-                strLine = it
-        } != null) {
-            result += strLine
+        contentResolver.openInputStream(this)!!.let { inputStream ->
+            val br = BufferedReader(InputStreamReader(inputStream))
+            var strLine: String?
+            while (br.readLine().also {
+                    strLine = it
+                } != null) {
+                result += strLine
+            }
+            inputStream.close()
+            Timber.v("Successfully read from file with uri: $this")
+            result
         }
-        inputStream.close()
-        Timber.v("Successfully read from file with uri: $this")
-        result
     } catch (e: IOException) {
         Timber.w("Unable to read from file with uri: $this", e)
         null
