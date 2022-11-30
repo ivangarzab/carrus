a2dc17a5cd3f3b987bad800879463b7489dd5342
Ivan Garza <ivangb6@gmail.com>
2022-11-29 18:31:34 -0800

    Reshuffle create fragment contents and implement IME navigation

--- app/src/main/java/com/ivangarzab/carrus/ui/create/CreateFragment.kt
+++ app/src/main/java/com/ivangarzab/carrus/ui/create/CreateFragment.kt
@@ -10,9 +10,6 @@ import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.transition.doOnEnd
-import androidx.core.transition.doOnResume
-import androidx.core.transition.doOnStart
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
