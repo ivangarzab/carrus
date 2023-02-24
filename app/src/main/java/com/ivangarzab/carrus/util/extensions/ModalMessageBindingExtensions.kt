e33ec08ef002f131ecf4a448fa85b1a5d6ac5d4f
Ivan Garza <ivangb6@gmail.com>
2023-02-23 20:27:47 -0800

    Continue wiring up the new messaging system for a single message

---
+++ app/src/main/java/com/ivangarzab/carrus/util/extensions/ModalMessageBindingExtensions.kt
@@ -0,0 +1,12 @@
+package com.ivangarzab.carrus.util.extensions
+
+import com.ivangarzab.carrus.databinding.ModalMessageBinding
+
+/**
+ * Created by Ivan Garza Bermea.
+ */
+fun ModalMessageBinding.bind(
+    message: String
+) {
+    this.message = message
+}
