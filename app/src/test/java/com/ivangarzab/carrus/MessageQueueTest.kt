7bf0064330d6ff100383ff17d7bd904a30293db8
Ivan Garza <ivangb6@gmail.com>
2023-02-27 22:25:28 -0800

    Finish implementing the MessageQueueTest class

--- app/src/test/java/com/ivangarzab/carrus/MessageQueueTest.kt
+++ app/src/test/java/com/ivangarzab/carrus/MessageQueueTest.kt
@@ -6,6 +6,8 @@ import com.ivangarzab.carrus.util.managers.MessageType
 import junit.framework.TestCase.assertEquals
 import org.junit.Before
 import org.junit.Test
+import java.lang.Boolean.FALSE
+import java.lang.Boolean.TRUE
 
 /**
  * Created by Ivan Garza Bermea.
@@ -20,31 +22,122 @@ class MessageQueueTest {
     }
 
     @Test
-    fun test_size_base() {
-        assertEquals(0, messageQueue.size())
-    }
-
-    @Test
     fun test_isNotEmpty_base() {
-        assertEquals(false, messageQueue.isNotEmpty())
-    }
-
-    @Test
-    fun test_size_add_one() {
-        messageQueue.add(TEST_MESSAGE_DATA)
-        assertEquals(1, messageQueue.size())
+        assertEquals(FALSE, messageQueue.isNotEmpty())
     }
 
     @Test
     fun test_isNotEmpty_add_one() {
-        messageQueue.add(TEST_MESSAGE_DATA)
-        assertEquals(true, messageQueue.isNotEmpty())
+        messageQueue.add(TEST_MESSAGE_DATA_1)
+        assertEquals(TRUE, messageQueue.isNotEmpty())
+    }
+
+    @Test
+    fun test_isNotEmpty_add_two() {
+        messageQueue.add(TEST_MESSAGE_DATA_1)
+        messageQueue.add(TEST_MESSAGE_DATA_1)
+        assertEquals(TRUE, messageQueue.isNotEmpty())
+    }
+
+    @Test
+    fun test_isNotEmpty_add_one_pop() {
+        messageQueue.add(TEST_MESSAGE_DATA_1)
+        messageQueue.pop()
+        assertEquals(FALSE, messageQueue.isNotEmpty())
+    }
+
+    @Test
+    fun test_size_base() {
+        assertEquals(ANSWER_0, messageQueue.size())
+    }
+
+    @Test
+    fun test_size_add_one() {
+        messageQueue.add(TEST_MESSAGE_DATA_1)
+        assertEquals(ANSWER_1, messageQueue.size())
+    }
+
+    @Test
+    fun test_size_add_one_pop() {
+        messageQueue.add(TEST_MESSAGE_DATA_1)
+        messageQueue.pop()
+        assertEquals(ANSWER_0, messageQueue.size())
+    }
+
+    @Test
+    fun test_size_add_two() {
+        messageQueue.add(TEST_MESSAGE_DATA_1)
+        messageQueue.add(TEST_MESSAGE_DATA_1)
+        assertEquals(ANSWER_2, messageQueue.size())
+    }
+
+    @Test
+    fun test_size_add_two_pop() {
+        messageQueue.add(TEST_MESSAGE_DATA_1)
+        messageQueue.add(TEST_MESSAGE_DATA_1)
+        messageQueue.pop()
+        assertEquals(ANSWER_1, messageQueue.size())
+    }
+
+    @Test(expected = NoSuchElementException::class)
+    fun test_pop_base() {
+        messageQueue.pop()
+    }
+
+    @Test
+    fun test_add_one_pop() {
+        messageQueue.add(TEST_MESSAGE_DATA_1)
+        assertEquals(
+            TEST_MESSAGE_DATA_1,
+            messageQueue.pop()
+        )
+    }
+
+    @Test
+    fun test_add_two_pop() {
+        messageQueue.add(TEST_MESSAGE_DATA_1) // first
+        messageQueue.add(TEST_MESSAGE_DATA_2) // second
+        assertEquals(
+            TEST_MESSAGE_DATA_1,
+            messageQueue.pop()
+        )
+    }
+
+    @Test
+    fun test_add_three_pop() {
+        messageQueue.add(TEST_MESSAGE_DATA_1) // first
+        messageQueue.add(TEST_MESSAGE_DATA_2) // second
+        messageQueue.add(MessageData(MessageType.INFO, "3")) // third
+        assertEquals(
+            TEST_MESSAGE_DATA_1,
+            messageQueue.pop()
+        )
+    }
+
+    @Test
+    fun test_add_three_pop_pop() {
+        messageQueue.add(TEST_MESSAGE_DATA_1) // first
+        messageQueue.add(TEST_MESSAGE_DATA_2) // second
+        messageQueue.add(MessageData(MessageType.INFO, "3")) // third
+        messageQueue.pop()
+        assertEquals(
+            TEST_MESSAGE_DATA_2,
+            messageQueue.pop()
+        )
     }
 
     companion object {
-        private val TEST_MESSAGE_DATA = MessageData(
+        private const val ANSWER_0 = 0
+        private const val ANSWER_1 = 1
+        private const val ANSWER_2 = 2
+
+        private val TEST_MESSAGE_DATA_1 = MessageData(
             type = MessageType.INFO,
-            text = "Test Message"
+            text = "One"
+        )
+        private val TEST_MESSAGE_DATA_2 = MessageData(
+            type = MessageType.WARNING,
+            text = "Two"
         )
     }
 }
