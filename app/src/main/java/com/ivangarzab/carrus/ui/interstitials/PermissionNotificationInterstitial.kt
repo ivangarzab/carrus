752cf0e492ed88ae1e94848fb25228d0bf65e21d
Ivan Garza <ivangb6@gmail.com>
2023-04-01 12:24:05 -0700

    Make the modal_interstitial_permission layout more flexible and conform the PermissionNotificationInterstitial class

--- app/src/main/java/com/ivangarzab/carrus/ui/interstitials/PermissionNotificationInterstitial.kt
+++ app/src/main/java/com/ivangarzab/carrus/ui/interstitials/PermissionNotificationInterstitial.kt
@@ -4,46 +4,26 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.DialogFragment
 import com.ivangarzab.carrus.R
-import com.ivangarzab.carrus.databinding.ModalInterstitialPermissionBinding
-import com.ivangarzab.carrus.util.extensions.clearBackgroundForRoundedCorners
 
 /**
  * Created by Ivan Garza Bermea.
  */
-class PermissionNotificationInterstitial : DialogFragment(
-    R.layout.modal_interstitial_permission
-) {
-
-    private lateinit var binding: ModalInterstitialPermissionBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = ModalInterstitialPermissionBinding.inflate(
-            inflater,
-            container,
-            false
-        )
-        return binding.root
-    }
+class PermissionNotificationInterstitial : PermissionInterstitial() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        this.dialog?.clearBackgroundForRoundedCorners()
-        binding.apply {
-            this.setSettingsClickListener { onSettingsClicked() }
-            this.setNotNowClickListener { dismiss() }
-        }
+        feedData(
+            PermissionInterstitialData(
+                title = R.string.missing_permission_interstitial_notification_title,
+                subtitle = R.string.missing_permission_interstitial_notification_subtitle,
+                body = R.string.missing_permission_interstitial_notification_content
+            )
+        )
     }
 
-    private fun onSettingsClicked() {
+    override fun onSettingsClicked() {
         startActivity(
             Intent().apply {
                 action = ACTION_APPLICATION_DETAILS_SETTINGS
