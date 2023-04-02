4ef79e89f9724767286d50df3587a2cd43def2ea
Ivan Garza <ivangb6@gmail.com>
2023-04-02 15:49:04 -0700

    Create UniqueMessageQueueTest class and start building testing assets into their own files

---
+++ app/src/test/java/com/ivangarzab/carrus/MessageDataTest.kt
@@ -0,0 +1,11 @@
+package com.ivangarzab.carrus
+
+import com.ivangarzab.carrus.data.MessageData
+import com.ivangarzab.carrus.data.MessageType
+
+/**
+ * Created by Ivan Garza Bermea.
+ */
+val TEST_MESSAGE_DATA_1 = MessageData(id = "1", type = MessageType.INFO, text = "One")
+val TEST_MESSAGE_DATA_2 = MessageData(id = "2", type = MessageType.WARNING, text = "Two")
+val TEST_MESSAGE_DATA_3 = MessageData(id = "3", type = MessageType.INFO, text = "Three")
