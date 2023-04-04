b91f0725ed0af9c611eef7bb99897b8ce8b585f9
Ivan Garza <ivangb6@gmail.com>
2023-04-03 19:08:53 -0700

    Fix Parcelize error with lint inside UniqueMessageQueue

--- app/src/main/java/com/ivangarzab/carrus/util/managers/UniqueMessageQueue.kt
+++ app/src/main/java/com/ivangarzab/carrus/util/managers/UniqueMessageQueue.kt
@@ -1,10 +1,12 @@
 package com.ivangarzab.carrus.util.managers
 
 import com.ivangarzab.carrus.data.MessageData
+import kotlinx.parcelize.Parcelize
 
 /**
  * Created by Ivan Garza Bermea.
  */
+@Parcelize
 class UniqueMessageQueue: MessageQueue() {
     override fun add(data: MessageData) {
         if (contains(data.id)) {
