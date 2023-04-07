1f8d6ea8878af208046422e183600f71b8ed3f06
Ivan Garza <ivangb6@gmail.com>
2023-04-06 21:05:45 -0700

    Move away from the MapView and instead aim to use the support fragment

--- app/src/main/java/com/ivangarzab/carrus/ui/map/MapFragment.kt
+++ app/src/main/java/com/ivangarzab/carrus/ui/map/MapFragment.kt
@@ -5,8 +5,8 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.ivangarzab.carrus.R
 import com.ivangarzab.carrus.databinding.FragmentMapBinding
 import com.ivangarzab.carrus.util.delegates.viewBinding
@@ -20,19 +20,32 @@ class MapFragment : Fragment(R.layout.fragment_map) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         Timber.d("Attempting to fetch map async")
-        binding.mapView.getMapAsync { googleMap ->
+        val mapFragment = requireActivity().supportFragmentManager.findFragmentById(
+            R.id.map_fragment
+        ) as? SupportMapFragment
+        mapFragment?.getMapAsync { googleMap ->
             Timber.d("Got an async map")
-            val sf = LatLng(37.7749, -122.4194)
-            googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
-            // Add a marker on the map coordinates.
-            /*googleMap.addMarker(
-                MarkerOptions()
-                    .position(sf)
-                    .title("San Francisco")
-            )*/
-            // Move the camera to the map coordinates and zoom in closer.
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sf))
-            googleMap.moveCamera(CameraUpdateFactory.zoomTo(15f))
+            setupMap(googleMap)
         }
+    }
+
+    private fun setupMap(googleMap: GoogleMap) {
+        Timber.d("Setting up map")
+        val sf = LatLng(37.7749, -122.4194)
+        googleMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+        // Add a marker on the map coordinates.
+        /*googleMap.addMarker(
+        MarkerOptions()
+            .position(sf)
+            .title("San Francisco")
+        )*/
+        // Move the camera to the map coordinates and zoom in closer.
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sf))
+        googleMap.moveCamera(CameraUpdateFactory.zoomTo(15f))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding.mapView.onDestroy()
     }
 }
