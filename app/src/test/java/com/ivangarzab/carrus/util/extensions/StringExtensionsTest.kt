6e4ed09b4e49c2a381e681aaed7f92b1229ee0c6
Ivan Garza <ivangb6@gmail.com>
2023-07-02 21:09:02 -0700

    Clean up

--- app/src/test/java/com/ivangarzab/carrus/util/extensions/StringExtensionsTest.kt
+++ app/src/test/java/com/ivangarzab/carrus/util/extensions/StringExtensionsTest.kt
@@ -10,13 +10,19 @@ class StringExtensionsTest {
 
     @Test
     fun test_getCalendarFromShortenerDate_success() {
-        assertThat(TEST_SHORTENED_DATE.getCalendarFromShortenedDate().timeInMillis)
+        assertThat(TEST_DATE.getCalendarFromShortenedDate().timeInMillis)
             .isEqualTo(ANSWER_SHORTENED_DATE)
     }
 
     @Test
     fun test_getCalendarFromShortenerDate_past_unix_time_failure() {
-        assertThat(TEST_SHORTENED_DATE_BAD.getCalendarFromShortenedDate().timeInMillis)
+        assertThat(TEST_DATE_PAST.getCalendarFromShortenedDate().timeInMillis)
+            .isLessThan(0L)
+    }
+
+    @Test
+    fun test_getCalendarFromShortenerDate_bad_date_failure() {
+        assertThat(TEST_DATE_BAD.getCalendarFromShortenedDate().timeInMillis)
             .isLessThan(0L)
     }
 
@@ -28,8 +34,9 @@ class StringExtensionsTest {
 
     companion object {
         private const val EMPTY_STRING = ""
-        private const val TEST_SHORTENED_DATE = "9/12/2023"
-        private const val TEST_SHORTENED_DATE_BAD = "13/42/1965"
+        private const val TEST_DATE = "9/12/2023"
+        private const val TEST_DATE_BAD = "13/42/111"
+        private const val TEST_DATE_PAST = "12/31/1869"
 
         private const val ANSWER_SHORTENED_DATE = 1697151600000
     }
