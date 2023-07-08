9017d3166bd984136ec8ba1111a9b05c455f42ba
Ivan Garza <ivangb6@gmail.com>
2023-07-07 19:13:35 -0700

    Get rid of all references to LeakCanary inside the App class for release builds

--- app/src/main/java/com/ivangarzab/carrus/util/managers/CrashlyticsLeakUploader.kt
+++ app/src/main/java/com/ivangarzab/carrus/util/managers/CrashlyticsLeakUploader.kt
@@ -4,6 +4,7 @@ import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.ktx.Firebase
 import com.ivangarzab.carrus.exceptions.MemoryLeakException
 import leakcanary.EventListener
+import leakcanary.LeakCanary
 import shark.Leak
 import shark.LeakTrace
 import shark.LeakTraceReference
@@ -19,62 +20,70 @@ import timber.log.Timber
  *
  * Created by Ivan Garza Bermea.
  */
-class CrashlyticsLeakUploader : EventListener {
+class LeakUploader {
+    fun setupCrashlyticsLeakUploader() {
+        LeakCanary.config = LeakCanary.config.run {
+            copy(eventListeners = eventListeners + CrashlyticsLeakUploader())
+        }
+    }
 
-    val crashlytics = Firebase.crashlytics
+    inner class CrashlyticsLeakUploader : EventListener {
 
-    override fun onEvent(event: EventListener.Event) {
-        if (event is EventListener.Event.HeapAnalysisDone.HeapAnalysisSucceeded) {
-            val heapAnalysis = event.heapAnalysis
-            val allLeakTraces = heapAnalysis
-                .allLeaks
-                .toList()
-                .flatMap { leak ->
-                    leak.leakTraces.map { leakTrace -> leak to leakTrace }
+        private val crashlytics = Firebase.crashlytics
+
+        override fun onEvent(event: EventListener.Event) {
+            if (event is EventListener.Event.HeapAnalysisDone.HeapAnalysisSucceeded) {
+                val heapAnalysis = event.heapAnalysis
+                val allLeakTraces = heapAnalysis
+                    .allLeaks
+                    .toList()
+                    .flatMap { leak ->
+                        leak.leakTraces.map { leakTrace -> leak to leakTrace }
+                    }
+                if (allLeakTraces.isEmpty().not()) {
+                    allLeakTraces.forEach { (leak, leakTrace) ->
+                        prepareLeakReport(leak, leakTrace)
+                    }
+                    sendAllLeakReports()
                 }
-            if (allLeakTraces.isEmpty().not()) {
-                allLeakTraces.forEach { (leak, leakTrace) ->
-                    prepareLeakReport(leak, leakTrace)
-                }
-                sendAllLeakReports()
             }
         }
-    }
 
-    private fun prepareLeakReport(leak: Leak, leakTrace: LeakTrace) = with(crashlytics) {
-        Timber.d("Preparing leak report(s) for Crashlytics")
-        val exception = leakTrace.asFakeException(
-            "${if (leak is LibraryLeak) "Library" else "Memory"} leaked on ${leak.shortDescription}."
-        )
+        private fun prepareLeakReport(leak: Leak, leakTrace: LeakTrace) = with(crashlytics) {
+            Timber.d("Preparing leak report(s) for Crashlytics")
+            val exception = leakTrace.asFakeException(
+                "${if (leak is LibraryLeak) "Library" else "Memory"} leaked on ${leak.shortDescription}."
+            )
 //        setCustomKey("Leak Trace", leakTrace.toString())
-        log("Leak Trace:\n\n$leakTrace")
-        recordException(exception)
-    }
-
-    private fun sendAllLeakReports() = with(crashlytics) {
-        Timber.d("Manually sending all unsent reports to Crashlytics")
-        sendUnsentReports()
-    }
-
-    private fun LeakTrace.asFakeException(message: String): Exception {
-        val exception = MemoryLeakException(message)
-        val stackTrace = mutableListOf<StackTraceElement>()
-        stackTrace.add(StackTraceElement("GcRoot", gcRootType.name, "GcRoot.kt", 42))
-        for (cause in referencePath) {
-            stackTrace.add(cause.buildStackTraceElement())
+            log("Leak Trace:\n\n$leakTrace")
+            recordException(exception)
         }
-        exception.stackTrace = stackTrace.toTypedArray()
-        return exception
-    }
 
-    private fun LeakTraceReference.buildStackTraceElement():
-            StackTraceElement {
-        val file = this.owningClassName.substringAfterLast(".") + ".kt"
-        return StackTraceElement(
-            this.owningClassName,
-            this.referenceDisplayName,
-            file,
-            42
-        )
+        private fun sendAllLeakReports() = with(crashlytics) {
+            Timber.d("Manually sending all unsent reports to Crashlytics")
+            sendUnsentReports()
+        }
+
+        private fun LeakTrace.asFakeException(message: String): Exception {
+            val exception = MemoryLeakException(message)
+            val stackTrace = mutableListOf<StackTraceElement>()
+            stackTrace.add(StackTraceElement("GcRoot", gcRootType.name, "GcRoot.kt", 42))
+            for (cause in referencePath) {
+                stackTrace.add(cause.buildStackTraceElement())
+            }
+            exception.stackTrace = stackTrace.toTypedArray()
+            return exception
+        }
+
+        private fun LeakTraceReference.buildStackTraceElement():
+                StackTraceElement {
+            val file = this.owningClassName.substringAfterLast(".") + ".kt"
+            return StackTraceElement(
+                this.owningClassName,
+                this.referenceDisplayName,
+                file,
+                42
+            )
+        }
     }
 }
