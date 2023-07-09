660467e15ac5144151211139489e0398da72796a
Ivan Garza <ivangb6@gmail.com>
2023-07-09 15:55:44 -0700

    Start setting up the Compose Theme using Material 3

---
+++ app/src/main/java/com/ivangarzab/carrus/ui/compose/theme/Common.kt
@@ -0,0 +1,7 @@
+package com.ivangarzab.carrus.ui.compose.theme
+
+/**
+ * This file contains the most common and basic Composables for use across the application.
+ *
+ * Created by Ivan Garza Bermea.
+ */
