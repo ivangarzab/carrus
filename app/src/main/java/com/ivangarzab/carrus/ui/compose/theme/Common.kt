c280c37bd10fa221ebd5e6d4b9f859f92f96bb3a
Ivan Garza <ivangb6@gmail.com>
2023-07-13 08:48:25 -0700

    Create PositiveButton & NegativeButton composables -  Fix Theme.kt to omit dynamic color, for now

--- app/src/main/java/com/ivangarzab/carrus/ui/compose/theme/Common.kt
+++ app/src/main/java/com/ivangarzab/carrus/ui/compose/theme/Common.kt
@@ -1,7 +1,51 @@
 package com.ivangarzab.carrus.ui.compose.theme
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
 /**
  * This file contains the most common and basic Composables for use across the application.
  *
  * Created by Ivan Garza Bermea.
  */
+
+@Preview
+@Composable
+fun PositiveButton(
+    modifier: Modifier = Modifier,
+    text: String = "ACTION",
+    onClick: () -> Unit = {  }
+) {
+    AppTheme {
+        Button(
+            modifier = modifier
+                .fillMaxWidth(),
+            onClick = { onClick() },
+        ) {
+            Text(text = text)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun NegativeButton(
+    modifier: Modifier = Modifier,
+    text: String = "Not Now",
+    onClick: () -> Unit = {  }
+) {
+    AppTheme {
+        TextButton(
+            modifier = modifier
+                .fillMaxWidth(),
+            onClick = { onClick() },
+        ) {
+            Text(text = text)
+        }
+    }
+}
--- app/src/main/java/com/ivangarzab/carrus/ui/compose/theme/Theme.kt
+++ app/src/main/java/com/ivangarzab/carrus/ui/compose/theme/Theme.kt
@@ -1,19 +1,15 @@
 package com.ivangarzab.carrus.ui.compose.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
@@ -88,17 +84,16 @@ private val DarkColorScheme = darkColorScheme(
 @Composable
 fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = true, // Android 12+
     content: @Composable () -> Unit
 ) {
     val view = LocalView.current
 
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        /*dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+        } TODO: Uncomment this block whenever we're ready to impl dynamic color support*/
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
