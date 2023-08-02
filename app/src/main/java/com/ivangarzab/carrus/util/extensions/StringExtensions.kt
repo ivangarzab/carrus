54902142aa57e9f49c6bc94cd7b24bc3efd0894b
Ivan Garza <ivangb6@gmail.com>
2023-08-01 17:49:16 -0700

    Fix the failing test

--- app/src/main/java/com/ivangarzab/carrus/util/extensions/StringExtensions.kt
+++ app/src/main/java/com/ivangarzab/carrus/util/extensions/StringExtensions.kt
@@ -9,13 +9,14 @@ import java.util.Locale
  * Created by Ivan Garza Bermea.
  */
 
-fun String.getCalendarFromShortenedDate(): Calendar = SimpleDateFormat(
-    "MM/dd/yy",
-    Locale.US
-).let { format ->
-    Calendar.getInstance().apply {
-        time = format.parse(this@getCalendarFromShortenedDate) as Date
+fun String.getCalendarFromShortenedDate(): Calendar = this.takeIf {
+    it.isNotBlank()
+}?.let {
+    SimpleDateFormat("MM/dd/yy", Locale.US).let { format ->
+        Calendar.getInstance().apply {
+            time = format.parse(this@getCalendarFromShortenedDate) as Date
+        }
     }
-}
+} ?: Calendar.getInstance().empty()
 
 fun String.parseIntoMoney(): Float = this.takeIf { it.isNotBlank() }?.toFloat() ?: 0.00f
