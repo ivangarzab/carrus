775377f4cb8c7e221fee826b7544af94d21bd7ee
Ivan Garza <ivangb6@gmail.com>
2023-10-01 12:41:01 -0700

    Expand on the Car.toString() by including a Car.toStringVerbose() function

---
+++ app/src/test/java/com/ivangarzab/carrus/util/managers/CarExporterTest.kt
@@ -0,0 +1,59 @@
+package com.ivangarzab.carrus.util.managers
+
+import com.google.common.truth.Truth.assertThat
+import com.ivangarzab.carrus.data.Car
+import org.junit.Test
+
+/**
+ * Created by Ivan Garza Bermea.
+ */
+class CarExporterTest {
+
+    private val exporter = CarExporter
+
+    @Test
+    fun test_exportFromJson_empty_car_data_not_null() {
+        assertThat(exporter.exportToJson(Car.empty))
+            .isNotNull()
+    }
+
+    @Test
+    fun test_exportFromJson_car_data_without_image_not_null() {
+        assertThat(exporter.exportToJson(Car.default))
+            .isNotNull()
+    }
+
+    @Test
+    fun test_exportFromJson_car_data_without_image_no_valid_imageUri() = with(
+        exporter.exportToJson(Car.default)
+    ) {
+        assertThat(this?.let { isCarJsonImageUriNull(it) })
+            .isTrue()
+    }
+
+    @Test
+    fun test_exportFromJson_car_data_with_image_not_null() = with(
+        exporter.exportToJson(Car.default.copy(imageUri = IMAGE_URI))
+    ) {
+        assertThat(this)
+            .isNotNull()
+    }
+
+    @Test
+    fun test_exportFromJson_car_data_with_image_no_valid_imageUri() = with(
+        exporter.exportToJson(Car.default.copy(imageUri = IMAGE_URI))
+    ) {
+        assertThat(this?.let { isCarJsonImageUriNull(it) })
+            .isTrue()
+    }
+
+    private fun isCarJsonImageUriNull(json: String): Boolean {
+        return if (json.contains("\"imageUri\"")) {
+            json.contains("\"imageUri\": null")
+        } else true
+    }
+
+    companion object {
+        private const val IMAGE_URI = "this_is_some_image_uri"
+    }
+}
