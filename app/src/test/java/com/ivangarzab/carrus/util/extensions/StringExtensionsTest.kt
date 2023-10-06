bc405bc80d32207f0a297eb3db15d7a20b93513d
Iván Garza Bermea <ivangb6@gmail.com>
2023-10-05 17:04:04 -0700

    First attempt at fixing the String.parseIntoMoney() crash (#140)
    * First attempt at fixing the String.parseIntoMoney() crash as found by the Play Store
    
    * Fix StringExtensions.parseIntoMoney() function so that all unit test can succeed
    -  Create a new String extension fun .isDigitsOnly() and add tests for this too

--- app/src/test/java/com/ivangarzab/carrus/util/extensions/StringExtensionsTest.kt
+++ app/src/test/java/com/ivangarzab/carrus/util/extensions/StringExtensionsTest.kt
@@ -34,12 +34,59 @@ class StringExtensionsTest {
             .isEqualTo(0L)
     }
 
+    @Test
+    fun test_isDigitsOnly_empty_string_failure() {
+        assertThat(EMPTY_STRING.isDigitsOnly())
+            .isFalse()
+    }
+
+    @Test
+    fun test_isDigitsOnly_valid_digits_success() {
+        assertThat(TEST_VALID_DIGITS.isDigitsOnly())
+            .isTrue()
+    }
+
+    @Test
+    fun test_isDigitsOnly_valid_digits_with_period_failure() {
+        assertThat(TEST_VALID_DIGITS_WITH_PERIOD.isDigitsOnly())
+            .isFalse()
+    }
+
+    @Test
+    fun test_parseIntoMoney_empty_string_failure() {
+        assertThat(EMPTY_STRING.parseIntoMoney())
+            .isEqualTo(EMPTY_MONEY_VALUE)
+    }
+
+    @Test
+    fun test_parseIntoMoney_non_digits_string_failure() {
+        assertThat(NON_DIGITS.parseIntoMoney())
+            .isEqualTo(EMPTY_MONEY_VALUE)
+    }
+
+    @Test
+    fun test_parseIntoMoney_valid_string_success() {
+        assertThat(TEST_VALID_DIGITS.parseIntoMoney())
+            .isEqualTo(ANSWER_VALID_DIGITS)
+    }
+
+    @Test
+    fun test_parseIntoMoney_valid_string_with_period_success() {
+        assertThat(TEST_VALID_DIGITS_WITH_PERIOD.parseIntoMoney())
+            .isEqualTo(ANSWER_VALID_DIGITS_WITH_PERIOD)
+    }
+
     companion object {
         private const val EMPTY_STRING = ""
         private const val TEST_DATE = "9/12/2023"
         private const val TEST_DATE_BAD = "13/42/111"
         private const val TEST_DATE_PAST = "12/31/1869"
-
         private const val ANSWER_SHORTENED_DATE = 1697151600000
+
+        private const val NON_DIGITS = "abc"
+        private const val TEST_VALID_DIGITS = "6"
+        private const val TEST_VALID_DIGITS_WITH_PERIOD = "999.99"
+        private const val ANSWER_VALID_DIGITS = 6.0f
+        private const val ANSWER_VALID_DIGITS_WITH_PERIOD = 999.99f
     }
 }
