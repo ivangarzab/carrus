bf12e1013335837ce75d249aca997f1926006aa4
Ivan Garza <ivangb6@gmail.com>
2023-10-22 11:20:17 -0700

    Create test for the new function inside the LiveDataTestUtil file: LiveData.getOrAwaitValue()

--- app/src/test/java/com/ivangarzab/carrus/LiveDataTestUtil.kt
+++ app/src/test/java/com/ivangarzab/carrus/LiveDataTestUtil.kt
@@ -12,10 +12,16 @@ import java.util.concurrent.TimeoutException
  */
 
 /**
- * Gets the value of a [LiveData] or waits for it to have one, with a timeout.
+ * The following function is meant to be used in order to easily and quickly get data from
+ * [LiveData] instances.  This function will take 1 out of the following 3 paths:
+ *  1.  If there's already a value, and no trigger defined, get that value
+ *  2.  If there's a trigger defined, wait for the trigger and attempt to get result afterwards
+ *  3.  If there's a trigger defined, wait for trigger.  If no new value is set, grab the current value, if available.
  *
+ * Gets the current value of a [LiveData] or waits for it to have one, with a timeout.
  * Use this extension from host-side (JVM) tests. It's recommended to use it alongside
  * `InstantTaskExecutorRule` or a similar mechanism to execute tasks synchronously.
+ *
  * As acquired from: https://developer.android.com/codelabs/advanced-android-kotlin-training-testing-basics/#8
  */
 @VisibleForTesting(otherwise = VisibleForTesting.NONE)
@@ -46,7 +52,9 @@ fun <T> LiveData<T>.getOrAwaitValue(
         trigger?.invoke()
         // Don't wait indefinitely if the LiveData is not set
         if (!latch.await(time, timeUnit)) {
-            throw TimeoutException("LiveData value didn't arrive in time.")
+            this.value?.let {
+                return it
+            } ?: throw TimeoutException("LiveData value didn't arrive in time.")
         }
     } finally {
         this.removeObserver(observer)
