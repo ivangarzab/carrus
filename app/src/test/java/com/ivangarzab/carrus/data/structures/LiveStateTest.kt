29c0df3254e0b50c8a1f56898d664e78cda0349a
Ivan Garza <ivangb6@gmail.com>
2023-10-22 19:45:43 -0700

    Finish LiveStateTest class, and create TestLifecycleOwner for support on this last push

--- app/src/test/java/com/ivangarzab/carrus/data/structures/LiveStateTest.kt
+++ app/src/test/java/com/ivangarzab/carrus/data/structures/LiveStateTest.kt
@@ -5,7 +5,8 @@ package com.ivangarzab.carrus.data.structures
  */
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
-import androidx.lifecycle.testing.TestLifecycleOwner
+import com.ivangarzab.carrus.TestLifecycleOwner
+import io.mockk.mockk
 import io.mockk.spyk
 import io.mockk.verify
 import org.junit.Before
@@ -17,432 +18,262 @@ class LiveStateTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var testLiveState: LiveState<String>
+    private lateinit var liveState: LiveState<String>
 
     private lateinit var owner: TestLifecycleOwner
 
     private lateinit var observer: Observer<String>
-    /*private var resultValue = TEST_DEFAULT_VALUE
-    private val observer = Observer<String> {
-        resultValue = it
-    }*/
 
     @Before
     fun setup() {
-        testLiveState = LiveState(INITIAL_VALUE)
+        liveState = LiveState(INITIAL_VALUE)
         owner = TestLifecycleOwner()
         observer = spyk()
     }
 
     @Test
     fun test_observe_base() {
-        testLiveState.observe(owner, observer)
+        liveState.observe(owner, observer)
         verify(atLeast = 0) {
             observer.onChanged(TEST_VALUE)
         }
     }
 
     @Test
-    fun test_observe_setState() {
-        testLiveState.observe(owner, observer)
-        testLiveState.setState { TEST_VALUE }
-        verify(atLeast = 1) {
+    fun test_observeForever_base() {
+        liveState.observeForever(observer)
+        verify(atLeast = 0) {
             observer.onChanged(TEST_VALUE)
         }
     }
 
     @Test
-    fun test_observeForever_base() {
-        testLiveState.observeForever(observer)
-        /*assertThat(resultValue)
-            .isEqualTo(INITIAL_VALUE)*/
-    }
-
-    /*
-    @Test
     fun `observe multi observers`() {
-//        val observer1: Observer<String> = mock(Observer::class)
-        testLiveState.observe(owner, observer)
-        testLiveState.observe(owner, observer1)
-        testLiveState.setState { TEST_VALUE }
-
-//        assertThat(result).isEqualTo(TEST_VALUE)
-        verify(observer, times(1)).onChanged(TEST_VALUE)
-        verify(observer1, times(1)).onChanged(TEST_VALUE)
+        owner.start()
+        val observer1: Observer<String> = spyk()
+        liveState.observe(owner, observer)
+        liveState.observe(owner, observer1)
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
+        verify(atLeast = 1) { observer1.onChanged(TEST_VALUE) }
     }
 
 
     @Test
     fun `observeForever multi observers`() {
-        // Given
-        val observer1 = mock<Observer<String>>()
-        testLiveState.observeForever(observer)
-        testLiveState.observeForever(observer1)
-
-        val event = "event"
-
-        // When
-        testLiveState.value = event
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
-        verify(observer1, times(1)).onChanged(event)
+        val observer1: Observer<String> = spyk()
+        liveState.observeForever(observer)
+        liveState.observeForever(observer1)
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
+        verify(atLeast = 1) { observer1.onChanged(TEST_VALUE) }
     }
 
     @Test
     fun `observe after start`() {
-        // Given
         owner.create()
-        testLiveState.observe(owner, observer)
-
-        val event = "event"
-
-        // When
-        testLiveState.value = event
-
-        // Then
-        verify(observer, never()).onChanged(event)
-
-        // When
+        liveState.observe(owner, observer)
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 0) { observer.onChanged(TEST_VALUE) }
         owner.start()
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
     }
 
     @Test
     fun `observe after emit event`() {
-        // Given
         owner.create()
-        val event = "event"
-        testLiveState.value = event
-        testLiveState.observe(owner, observer)
-
-        // When
+        liveState.setState { TEST_VALUE }
+        liveState.observe(owner, observer)
         owner.start()
-
-        // Then
-        if (config == testLiveStateConfig.Normal) {
-            verify(observer, never()).onChanged(event)
-        } else {
-            verify(observer, times(1)).onChanged(event)
-        }
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
     }
 
     @Test
     fun `observe after start with multi observers`() {
-        // Given
         owner.create()
-        val observer1 = mock<Observer<String>>()
-        testLiveState.observe(owner, observer)
-        testLiveState.observe(owner, observer1)
+        val observer1: Observer<String> = spyk()
+        liveState.observe(owner, observer)
+        liveState.observe(owner, observer1)
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 0) { observer.onChanged(TEST_VALUE) }
+        verify(atLeast = 0) { observer1.onChanged(TEST_VALUE) }
 
-        val event = "event"
-
-        // When
-        testLiveState.value = event
-
-        // Then
-        verify(observer, never()).onChanged(event)
-        verify(observer1, never()).onChanged(event)
-
-        // When
         owner.start()
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
-        verify(observer1, times(1)).onChanged(event)
+        verify(atLeast = 0) { observer.onChanged(TEST_VALUE) }
+        verify(atLeast = 0) { observer1.onChanged(TEST_VALUE) }
     }
 
     @Test
     fun `observe after stop`() {
-        // Given
         owner.stop()
-        testLiveState.observe(owner, observer)
-
-        val event = "event"
-
-        // When
-        testLiveState.value = event
-
-        // Then
-        verify(observer, never()).onChanged(event)
+        liveState.observe(owner, observer)
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 0) { observer.onChanged(TEST_VALUE) }
     }
 
     @Test
     fun `observe after start again`() {
-        // Given
         owner.stop()
-        testLiveState.observe(owner, observer)
+        liveState.observe(owner, observer)
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 0) { observer.onChanged(TEST_VALUE) }
 
-        val event = "event"
-
-        // When
-        testLiveState.value = event
-
-        // Then
-        verify(observer, never()).onChanged(event)
-
-        // When
         owner.start()
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
     }
 
     @Test
     fun `observe after one observation`() {
-        // Given
         owner.start()
-        testLiveState.observe(owner, observer)
-
-        val event = "event"
-
-        // When
-        testLiveState.value = event
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
-
-        // When
+        liveState.observe(owner, observer)
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
         owner.stop()
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
-
-        // When
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
         owner.start()
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
     }
 
     @Test
     fun `observe after one observation multi owner`() {
-        // Given
         owner.start()
-        testLiveState.observe(owner, observer)
+        liveState.observe(owner, observer)
         val owner1 = TestLifecycleOwner()
-        val observer1 = mock<Observer<String>>()
+        val observer1: Observer<String> = spyk()
         owner1.start()
 
-        val event = "event"
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
 
-        // When
-        testLiveState.value = event
+        liveState.observe(owner1, observer1)
+        verify(atLeast = 0) { observer1.onChanged(TEST_VALUE) }
 
-        // Then
-        verify(observer, times(1)).onChanged(event)
-
-        // Given
-        testLiveState.observe(owner1, observer1)
-
-        // Then
-        verify(observer1, never()).onChanged(event)
-
-        // When
-        testLiveState.value = event
-
-        // Then
-        verify(observer, times(2)).onChanged(event)
-        verify(observer1, times(1)).onChanged(event)
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 2) { observer.onChanged(TEST_VALUE) }
+        verify(atLeast = 1) { observer1.onChanged(TEST_VALUE) }
     }
 
     @Test
     fun `observe after one observation with new owner`() {
-        // Given
         owner.start()
-        testLiveState.observe(owner, observer)
+        liveState.observe(owner, observer)
 
-        val event = "event"
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
 
-        // When
-        testLiveState.value = event
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
-
-        // When
         owner.destroy()
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
 
-        // Then
-        verify(observer, times(1)).onChanged(event)
-
-        // When
         owner = TestLifecycleOwner()
-        observer = mock()
-        testLiveState.observe(owner, observer)
+        observer = mockk()
+        liveState.observe(owner, observer)
         owner.start()
-
-        // Then
-        verify(observer, never()).onChanged(event)
+        verify(atLeast = 0) { observer.onChanged(TEST_VALUE) }
     }
 
     @Test
     fun `observe after one observation with new owner after start`() {
-        // Given
         owner.start()
-        testLiveState.observe(owner, observer)
+        liveState.observe(owner, observer)
 
-        val event = "event"
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
 
-        // When
-        testLiveState.value = event
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
-
-        // When
         owner.destroy()
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
 
-        // Then
-        verify(observer, times(1)).onChanged(event)
-
-        // When
         owner = TestLifecycleOwner()
-        observer = mock()
+        observer = mockk()
         owner.start()
-        testLiveState.observe(owner, observer)
-
-        // Then
-        verify(observer, never()).onChanged(event)
+        liveState.observe(owner, observer)
+        verify(atLeast = 0) { observer.onChanged(TEST_VALUE) }
     }
 
     @Test
     fun `observe after remove`() {
-        // Given
         owner.start()
-        testLiveState.observe(owner, observer)
+        liveState.observe(owner, observer)
 
-        val event = "event"
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
 
-        // When
-        testLiveState.value = event
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
-
-        // When
-        testLiveState.removeObserver(observer)
-        testLiveState.value = event
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
+        liveState.removeObserver(observer)
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
     }
 
     @Test
     fun `observe after remove before emit`() {
-        // Given
         owner.start()
-        testLiveState.observe(owner, observer)
-        testLiveState.removeObserver(observer)
-
-        val event = "event"
-
-        // When
-        testLiveState.value = event
-
-        // Then
-        verify(observer, never()).onChanged(event)
+        liveState.observe(owner, observer)
+        liveState.removeObserver(observer)
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 0) { observer.onChanged(TEST_VALUE) }
     }
 
     @Test
     fun `observe after remove owner`() {
-        // Given
         owner.start()
-        testLiveState.observe(owner, observer)
+        liveState.observe(owner, observer)
 
-        val event = "event"
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
 
-        // When
-        testLiveState.value = event
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
-
-        // When
-        testLiveState.removeObservers(owner)
-        testLiveState.value = event
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
+        liveState.removeObservers(owner)
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
     }
 
     @Test
     fun `observe after remove owner before emit`() {
-        // Given
         owner.start()
-        testLiveState.observe(owner, observer)
-        testLiveState.removeObservers(owner)
+        liveState.observe(owner, observer)
+        liveState.removeObservers(owner)
 
-        val event = "event"
-
-        // When
-        testLiveState.value = event
-
-        // Then
-        verify(observer, never()).onChanged(event)
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 0) { observer.onChanged(TEST_VALUE) }
     }
 
     @Test
     fun `observe after remove multi owner`() {
-        // Given
         owner.start()
-        testLiveState.observe(owner, observer)
+        liveState.observe(owner, observer)
         val owner1 = TestLifecycleOwner()
-        val observer1 = mock<Observer<String>>()
+        val observer1: Observer<String> = spyk()
         owner1.start()
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
+        verify(atLeast = 0) { observer1.onChanged(TEST_VALUE) }
 
-        val event = "event"
-
-        // When
-        testLiveState.value = event
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
-        verify(observer1, never()).onChanged(event)
-
-        // When
-        testLiveState.observe(owner1, observer1)
-        testLiveState.removeObserver(observer)
-        testLiveState.value = event
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
-        verify(observer1, times(1)).onChanged(event)
+        liveState.observe(owner1, observer1)
+        liveState.removeObserver(observer)
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
+        verify(atLeast = 1) { observer1.onChanged(TEST_VALUE) }
     }
 
     @Test
     fun `observe after remove owner multi owner`() {
-        // Given
         owner.start()
-        testLiveState.observe(owner, observer)
+        liveState.observe(owner, observer)
         val owner1 = TestLifecycleOwner()
-        val observer1 = mock<Observer<String>>()
+        val observer1: Observer<String> = spyk()
         owner1.start()
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
+        verify(atLeast = 0) { observer1.onChanged(TEST_VALUE) }
 
-        val event = "event"
-
-        // When
-        testLiveState.value = event
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
-        verify(observer1, never()).onChanged(event)
-
-        // When
-        testLiveState.observe(owner1, observer1)
-        testLiveState.removeObservers(owner)
-        testLiveState.value = event
-
-        // Then
-        verify(observer, times(1)).onChanged(event)
-        verify(observer1, times(1)).onChanged(event)
-    }*/
+        liveState.observe(owner1, observer1)
+        liveState.removeObservers(owner)
+        liveState.setState { TEST_VALUE }
+        verify(atLeast = 1) { observer.onChanged(TEST_VALUE) }
+        verify(atLeast = 1) { observer1.onChanged(TEST_VALUE) }
+    }
 
     companion object {
         private const val TEST_DEFAULT_VALUE = ""
         private const val INITIAL_VALUE = "initialValue"
         private const val TEST_VALUE = "test-value"
+        private const val TEST_VALUE_2 = "testValue"
     }
 }
