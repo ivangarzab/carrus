be6f053e2d996b49ffba9bab5712667e23c712ea
Ivan Garza <ivangb6@gmail.com>
2023-10-24 18:54:27 -0700

    Duplicate the MessageQueueRepositoryTest class into the unit test folder as it doesn't need a double to be tested locally

--- app/src/test/java/com/ivangarzab/carrus/Answers.kt
+++ app/src/test/java/com/ivangarzab/carrus/Answers.kt
@@ -6,3 +6,10 @@ package com.ivangarzab.carrus
 const val ANSWER_0 = 0
 const val ANSWER_1 = 1
 const val ANSWER_2 = 2
+const val ANSWER_3 = 3
+const val ANSWER_4 = 4
+const val ANSWER_5 = 5
+const val ANSWER_6 = 6
+const val ANSWER_7 = 7
+const val ANSWER_8 = 8
+const val ANSWER_9 = 9
