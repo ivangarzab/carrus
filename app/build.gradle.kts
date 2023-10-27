de9a7af081626d65299705b2b7fa54e0c7e3053b
Ivan Garza <ivangb6@gmail.com>
2023-10-26 21:48:42 -0700

    Update versionCode & versionName for release v1.0.0-alpha11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.ivangarzab.carrus"
         minSdk = 26
         targetSdk = 33
-        versionCode = 10
-        versionName = "1.0.0-alpha10"
+        versionCode = 11
+        versionName = "1.0.0-alpha11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
