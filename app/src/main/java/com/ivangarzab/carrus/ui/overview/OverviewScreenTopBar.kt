aadb94052d0a0ea8179cfb6d11a398b24c908c72
Ivan Garza Bermea <ivangb6@gmail.com>
2023-11-15 23:22:04 -0800

    Polish Coil integration, and fix overview topbar as well

--- app/src/main/java/com/ivangarzab/carrus/ui/overview/OverviewScreenTopBar.kt
+++ app/src/main/java/com/ivangarzab/carrus/ui/overview/OverviewScreenTopBar.kt
@@ -2,6 +2,7 @@ package com.ivangarzab.carrus.ui.overview
 
 import android.content.res.Configuration
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
@@ -14,19 +15,23 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.constraintlayout.compose.ConstrainedLayoutReference
 import androidx.constraintlayout.compose.ConstraintLayout
-import coil.compose.AsyncImage
+import coil.compose.AsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
-import com.ivangarzab.carrus.ui.compose.fadingEdge
+import coil.size.Size
 import com.ivangarzab.carrus.ui.compose.theme.AppTheme
+import timber.log.Timber
 import java.lang.Float.min
 
 /**
@@ -50,42 +55,42 @@ fun OverviewScreenTopBar(
         ConstraintLayout {
             val image: ConstrainedLayoutReference = createRef()
             val topbar: ConstrainedLayoutReference = createRef()
+            val painter = rememberAsyncImagePainter(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(data = imageUri)
+                    .size(Size.ORIGINAL) // Set the target size to load the image at.
+                    .listener { request, result ->
+                        Timber.d("Image request result: $result")
+                    }
+                    .build()
+            )
 
-            imageUri?.let {
-                AsyncImage(
+            if (painter.state is AsyncImagePainter.State.Success) {
+                Timber.d("imageUri=$imageUri")
+                Image(
                     modifier = Modifier
                         .constrainAs(image) {
+                            start.linkTo(topbar.start)
+                            end.linkTo(topbar.end)
                             bottom.linkTo(topbar.bottom)
                         }
                         .fillMaxWidth()
                         .graphicsLayer {
-                            val scrollState = scrollBehavior.state
-                            alpha = 1f - ((scrollState.heightOffset /
-                                    scrollState.heightOffsetLimit) * 1.2f)
-                            translationY = 0.2f * scrollState.heightOffset
+                            with(scrollBehavior.state) {
+                                Timber.d("heightOffset=%${heightOffset} | heightOffsetLimit=%${heightOffsetLimit}")
+                                alpha = 1f - ((heightOffset / heightOffsetLimit) * 1.2f)
+                            }
                         },
-                    model = ImageRequest
-                        .Builder(LocalContext.current)
-                        .data(data = imageUri)
-                        .build(),
-                    contentScale = ContentScale.Crop, //TODO: Create custom ContentScale
-                    contentDescription = "Top App Bar background image"
+                    painter = painter,
+                    contentScale = ContentScale.Crop,
+                    alignment = Alignment.Center,
+                    colorFilter = ColorFilter.tint(topbarColor, BlendMode.Color),
+                    contentDescription = "Top App Bar background image",
                 )
             }
             LargeTopAppBar(
                 modifier = Modifier
                     .constrainAs(topbar) { /* No-op */ }
-                    .fadingEdge(
-                        Brush.verticalGradient(
-                            0.90f to topbarColor,
-                            0.95f to topbarColor.copy(
-                                alpha = 0.90f
-                            ),
-                            1.0f to topbarColor.copy(
-                                alpha = 0.80f
-                            )
-                        )
-                    )
                     .combinedClickable(
                         onClick = { },
                         onLongClick = addTestMessage
@@ -101,15 +106,13 @@ fun OverviewScreenTopBar(
                 },
                 colors = TopAppBarDefaults.largeTopAppBarColors(
                     containerColor = imageUri?.let {
-                        val scrollState = scrollBehavior.state
-                        topbarColor.copy(
-                            alpha = min(
-                                1f,
-                                (scrollState.heightOffset / scrollState.heightOffsetLimit)
-                                        + 0.4f
+                        with(scrollBehavior.state) {
+                            topbarColor.copy(
+                                alpha = min(1f, (heightOffset / heightOffsetLimit) + 0.4f)
                             )
-                        )
+                        }
                     } ?: topbarColor
+
                 ),
                 actions = actions,
                 scrollBehavior = scrollBehavior
