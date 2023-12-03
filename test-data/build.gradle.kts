545b74b767133a1aa520006b0ecc15b2f1f62279
Ivan Garza <ivangb6@gmail.com>
2023-12-02 17:19:45 -0800

    Create the new :test-data module and start setting up dependencies

--- test-data/build.gradle.kts
+++ test-data/build.gradle.kts
@@ -2,11 +2,10 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    id("kotlin-kapt")
 }
 
 android {
-    namespace = "com.ivangarzab.analytics"
+    namespace = "com.ivangarzab.test_data"
     compileSdk = 33
 
     defaultConfig {
@@ -32,38 +31,13 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-    packaging {
-        resources.excludes.add("META-INF/LICENSE.md")
-        resources.excludes.add("META-INF/LICENSE-notice.md")
-    } // This block fixes a bug for the :analytics:mergeDebugAndroidTestJavaResources Gradle task
 }
 
 dependencies {
+    compileOnly(project(":app"))
     implementation(libs.androidx.core)
     implementation(libs.app.compat)
-
-    implementation(libs.timber)
-
-    implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.crashlytics)
-    implementation(libs.firebase.analytics)
-
-    implementation(libs.hilt)
-    kapt(libs.hilt.compiler)
-    testImplementation(libs.hilt.testing)
-    kaptTest(libs.hilt.compiler)
-    androidTestImplementation(libs.hilt.testing)
-    kaptAndroidTest(libs.hilt.compiler)
-
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.espresso)
-
-    testImplementation(libs.truth)
-    androidTestImplementation(libs.truth)
-
-    testImplementation(libs.mockk)
-    testImplementation(libs.mockk.agent)
-    androidTestImplementation(libs.mockk)
-    androidTestImplementation(libs.mockk.agent)
 }
