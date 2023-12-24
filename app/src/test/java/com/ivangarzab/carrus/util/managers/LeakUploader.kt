eba433fa80453d601db4879660dafa7fee52a50e
Ivan Garza <ivangb6@gmail.com>
2023-12-23 22:00:19 -0800

    Start getting the App.kt file tested, and further broken down

---
+++ app/src/test/java/com/ivangarzab/carrus/util/managers/LeakUploader.kt
@@ -0,0 +1,10 @@
+package com.ivangarzab.carrus.util.managers
+
+/**
+ * Created by Ivan Garza Bermea.
+ */
+class LeakUploader {
+    fun setupCrashlyticsLeakUploader() {
+        // No-op
+    }
+}
