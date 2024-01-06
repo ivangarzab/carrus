aeab6ebc5b9a828bc3e9a8518ede97f26dc0abf4
ivangarzab <ivangb6@gmail.com>
2024-01-05 22:26:20 -0800

    Create two more flows to fill up the service list

--- app/src/main/java/com/ivangarzab/carrus/ui/modals/ServiceModalScreen.kt
+++ app/src/main/java/com/ivangarzab/carrus/ui/modals/ServiceModalScreen.kt
@@ -1,6 +1,8 @@
 package com.ivangarzab.carrus.ui.modals
 
 import android.content.res.Configuration
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -12,9 +14,13 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.testTagsAsResourceId
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -124,7 +130,11 @@ fun ServiceModalScreen(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(
+    ExperimentalMaterial3Api::class,
+    ExperimentalFoundationApi::class,
+    ExperimentalComposeUiApi::class
+)
 @Preview
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
@@ -144,11 +154,47 @@ fun CalendarDialog(
             onDismissed = onDismissed
         ) {
             Text(
+                modifier = Modifier
+                    .combinedClickable(
+                        onClick = { },
+                        onLongClick = {
+                            //TODO: Debug check
+                            onValueSelected(
+                                Calendar.getInstance().apply {
+                                    add(Calendar.MONTH, 1)
+                                }
+                            )
+                            onDismissed()
+                        }
+                    ),
                 text = title,
                 style = Typography.titleMediumLarge
             )
             DatePicker(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .semantics { testTagsAsResourceId = true }.testTag("Date Picker")
+                    .combinedClickable(
+                        onClick = {
+                            //TODO: Debug check
+                            onValueSelected(
+                                Calendar.getInstance().apply {
+                                    add(Calendar.MONTH, -1)
+                                }
+                            )
+                            onDismissed()
+                        },
+                        onLongClick = {
+                            //TODO: Debug check
+                            onValueSelected(
+                                Calendar.getInstance().apply {
+                                    add(Calendar.MONTH, -1)
+                                    add(Calendar.DAY_OF_MONTH, 1)
+                                }
+                            )
+                            onDismissed()
+                        }
+                    ),
                 state = datePickerState
             )
             PositiveButton(
